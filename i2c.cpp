1e26711478172c51bc2c580a519876c13996607b
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 13:44:32 +0900

    Update i2c.cpp

--- i2c.cpp
+++ i2c.cpp
@@ -3,11 +3,6 @@
 namespace I2CEx {
 
     void _setFrequency(int hz) {
-
-        if (hz <= 100000) hz = 100000;
-        else if (hz <= 250000) hz = 250000;
-        else hz = 400000;
-
-        uBit.i2c.setFrequency(hz);
+        // 何もしない
     }
 }
