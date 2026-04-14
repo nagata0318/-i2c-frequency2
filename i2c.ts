325326f881f6994d4fcd259bbbf8bf51463355c6
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 13:40:13 +0900

    Update i2c.ts

--- i2c.ts
+++ i2c.ts
@@ -2,16 +2,32 @@
 namespace I2CEx {
 
     /**
-     * I2C周波数設定
+     * I2C周波数を設定
+     */
+    //% block="I2C周波数を %freq に設定"
+    export function setFrequency(freq: I2CFreq): void {
+        _setFrequency(freq)
+    }
+
+    /**
+     * 任意周波数設定（上級者向け）
      */
     //% block="I2C周波数を %hz Hz に設定"
-    export function setFrequency(hz: number): void {
+    export function setFrequencyRaw(hz: number): void {
         _setFrequency(hz)
     }
 
-    // shim関数（これが重要）
+    // shim
     //% shim=I2CEx::_setFrequency
     function _setFrequency(hz: number): void {
         return
+    }
+
+    // 周波数選択
+    export enum I2CFreq {
+        //% block="100kHz"
+        F100K = 100000,
+        //% block="400kHz"
+        F400K = 400000
     }
 }
